5dbc89ffd274591ed4468de9868ff9b7a9d9d95b
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-02-16 14:02:07 +0100

    update listing blade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+        Listing::create([
+            'title'=>'FullStack web developer',
+            'tags'=>'html,css,js,php',
+            'company'=>'Happysterdam',
+            'location'=>'Algiers Algeria',
+            'email'=>'ouadijh@mail.com',
+            'website'=>'https://ouadjihboudraa.vercel.app',
+            'description'=>'lorem ipisumlorem ipisumlorem ipisumlorem
+            ipisumlorem ipisumlorem ipisumlorem
+             ipisumlorem ipisumlorem ipisum'
+        ]);
+        Listing::create([
+            'title'=>'Graphic Designer',
+            'tags'=>'adobe ps ae xd...,figma,ux/ui',
+            'company'=>'Happysterdam',
+            'location'=>'Algiers Algeria',
+            'email'=>'ouadijh@happysterdam.dz',
+            'website'=>'ouadjihboudraa.vercel.app',
+            'description'=>'lorem ipisumlorem ipisumlorem ipisumlorem ipisumlorem ipisumlorem ipisumlorem ipisumlorem ipisumlorem ipisum'
+        ]);
+        Listing::factory(5)->create();
+
     }
 }
