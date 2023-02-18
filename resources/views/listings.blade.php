32eda9205fae8b1964a663b0e21645a3a27141ba
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-02-18 06:56:25 +0100

    update layout blade

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,43 @@
+@extends('layout')
+
+@section('content')
+<div
+class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+@if(count($listings) == 0)
+<p>No listnings available</p>
+@else
+@foreach($listings as $listing)
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="images/no-image.png"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="listings/{{$listing->id}}"> {{$listing->title}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4"> {{$listing->company}}</div>
+            <ul class="flex">
+
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#"> {{$listing->tags}}</a>
+                </li>
+            </ul>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+            </div>
+        </div>
+    </div>
+</div>
+{{--<h2>
+    <a href="/wasfa/server.php/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+</h2>
+<p>{{$listing['description']}}</p>--}}
+@endforeach
+@endif
+</div>
+@endsection
